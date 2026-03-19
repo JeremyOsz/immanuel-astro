04048d6d4f513c80d44aebafa0ab0a24d514a827
Jeremy Osztreicher <j.osztreicher@gmail.com>
2026-03-19 12:00:03 +0000

    set up for serverless deployment

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
     
     # Environment
     ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
-    
+
     # Server settings
     HOST = os.getenv("HOST", "0.0.0.0")
     PORT = int(os.getenv("PORT", "8000"))
